9f10e61d8e8541c9b0c5b68640b73f779cbc462b
DaoNV <zendobk@gmail.com>
2016-01-10 22:00:24 +0700

    init

---
+++ RealmS/RealmS.h
@@ -0,0 +1,19 @@
+//
+//  RealmS.h
+//  RealmS
+//
+//  Created by DaoNV on 1/10/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmS.
+FOUNDATION_EXPORT double RealmSVersionNumber;
+
+//! Project version string for RealmS.
+FOUNDATION_EXPORT const unsigned char RealmSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmS/PublicHeader.h>
+
+
