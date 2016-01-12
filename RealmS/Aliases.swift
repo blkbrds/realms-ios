4558deb8bec1c2dd0d37a44cbdd4d00873f26cfb
DaoNV <zendobk@gmail.com>
2016-01-12 11:37:54 +0700

    forward public type

---
+++ RealmS/Aliases.swift
@@ -0,0 +1,26 @@
+//
+//  Aliases.swift
+//  RealmS
+//
+//  Created by DaoNV on 1/12/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import RealmSwift
+
+public typealias AnyRealmCollection = RealmSwift.AnyRealmCollection
+public typealias Configuration = Realm.Configuration
+public typealias DynamicObject = RealmSwift.DynamicObject
+public typealias List = RealmSwift.List
+public typealias ListBase = RealmSwift.ListBase
+public typealias Migration = RealmSwift.Migration
+public typealias Object = RealmSwift.Object
+public typealias ObjectSchema = RealmSwift.ObjectSchema
+public typealias Property = RealmSwift.Property
+public typealias Realm = RealmSwift.Realm
+public typealias RealmCollectionType = RealmSwift.RealmCollectionType
+public typealias RealmOptional = RealmSwift.RealmOptional
+public typealias Results = RealmSwift.Results
+public typealias ResultsBase = RealmSwift.ResultsBase
+public typealias Schema = RealmSwift.Schema
+public typealias SortDescriptor = RealmSwift.SortDescriptor
