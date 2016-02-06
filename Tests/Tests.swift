054258298123fc714fef6d6058e48d767ccae2c5
DaoNV <zendobk@gmail.com>
2016-02-07 00:56:58 +0700

    add circle.yml

---
+++ Tests/Tests.swift
@@ -0,0 +1,38 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by DaoNV on 2/6/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import XCTest
+
+class Tests: XCTestCase {
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testRealmS() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
+  func testPerformanceRealmS() {
+    // This is an example of a performance test case.
+    self.measureBlock {
+      // Put the code you want to measure the time of here.
+    }
+  }
+  
+  func testObjectMapper() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
