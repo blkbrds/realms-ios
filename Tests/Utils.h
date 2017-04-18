8d940620c9a16c398c7fe4797dcf75a48187a5b8
DaoNV <zendobk@gmail.com>
2017-04-18 12:11:14 +0700

    make all old tests run successfully

---
+++ Tests/Utils.h
@@ -0,0 +1,11 @@
+//
+//  Utils.h
+//  RealmS
+//
+//  Created by DaoNV on 4/18/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void DispatchOnce(void(^block)());
